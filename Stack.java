903e545d743f11eb684c9f2d51f4216af18c05c7
drisyaathul <drisyak809@gmail.com>
2023-04-28 22:51:57 +0800

    Stacks

---
+++ Stack.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.StacksAndQueues;
+
+public class Stack {
+
+}
---
+++ StackMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.StacksAndQueues;
+
+public class StackMain {
+    public static void main(String[] args) {
+        System.out.println("*** Welcome to Stack *** ");
+    }
+}
