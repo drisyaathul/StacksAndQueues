69c01dcf91b7e56dad7f61cb8c0c5edc9de4ee33
drisyaathul <drisyak809@gmail.com>
2023-04-28 23:11:42 +0800

    Added: Created a Stack of 56,30 and 70

--- Stack.java
+++ Stack.java
@@ -1,5 +1,21 @@
 package com.bridgelabz.StacksAndQueues;
 
-public class Stack {
+import com.bridgelabz.LinkedList;
 
+public class Stack<T> {
+    //creates Linked list Object
+    LinkedList<T> linkedList = new LinkedList<>();
+
+    public void push(T data) {
+        /*
+          Pushing the nodes into Stack using Linked list
+         */
+        linkedList.push(data);
+    }
+    public void display() {
+        /*
+          Display the LinkedList
+         */
+        linkedList.display();
+    }
 }
--- StackMain.java
+++ StackMain.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.StacksAndQueues;
 
+import com.bridgelabz.LinkedList;
+
 public class StackMain {
     public static void main(String[] args) {
         System.out.println("*** Welcome to Stack *** ");
+        /*
+        UC1: create a Stack of 56->30->70
+         */
+        Stack<Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.display();
     }
 }
