510cb4bef745eafed5a93a8c841e97847e2713de
drisyaathul <drisyak809@gmail.com>
2023-04-29 08:35:57 +0800

    Added: Peek and pop from Stack till it is empty

--- Stack.java
+++ Stack.java
@@ -18,4 +18,19 @@ public class Stack<T> {
          */
         linkedList.display();
     }
+    public void peek() {
+        /*
+         * Peek returns the top item but does not remove it.
+         * First Element is popped and pushed to the same linkedList.
+         */
+        Integer peekedData = (Integer) linkedList.pop();
+        System.out.println("Peek Element is "+peekedData);
+        linkedList.push((T)peekedData);
+    }
+    public T pop() {
+        /*
+           pop to delete the first element
+         */
+        return linkedList.pop();
+    }
 }
--- StackMain.java
+++ StackMain.java
@@ -13,5 +13,26 @@ public class StackMain {
         stack.push(30);
         stack.push(56);
         stack.display();
+        System.out.println("---------------------------------------");
+        /*
+        UC2: Peek and Pop from the stack till it is Empty
+         */
+        stack.display();
+        stack.peek();
+        stack.display();
+        System.out.println("---------------------------------------");
+        stack.display();
+        Integer poppedData;
+        do {
+            poppedData = stack.pop();
+            if (poppedData == null)
+                System.out.println("Stack is Empty");
+            else
+                System.out.println("Popped element is " + poppedData);
+            stack.display();
+        }
+        while (poppedData != null);
+        System.out.println("---------------------------------------");
     }
 }
+
